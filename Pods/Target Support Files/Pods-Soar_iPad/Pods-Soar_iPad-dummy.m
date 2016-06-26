a6bd694fd42a26d6955534ba758e3d9e9036ba1f
BHigh17 <brandonhightower@Brandons-MacBook-Pro.local>
2016-06-26 02:56:41 -0400

    initial commit

---
+++ Pods/Target Support Files/Pods-Soar_iPad/Pods-Soar_iPad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Soar_iPad : NSObject
+@end
+@implementation PodsDummy_Pods_Soar_iPad
+@end
---
+++ Pods/Target Support Files/Pods-Soar_iPad/Pods-Soar_iPad-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Soar_iPadVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Soar_iPadVersionString[];
+
